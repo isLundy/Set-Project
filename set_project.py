0f1294271801252a38282a328608e5b7d8fc83a9
Lundy Hu <lundyhu@outlook.com>
2024-04-27 21:26:58 +0800

    Update set_project.py

--- set_project.py
+++ set_project.py
@@ -1,16 +1,21 @@
 def set_project():
+
+    # check node numbers
     if len(nuke.selectedNodes()) == 1:
+        # get node info
         node = nuke.selectedNode()
         first_frame = 1001
         last_frame = int(node['origlast'].getValue() - node['origfirst'].getValue() + first_frame)
         new_width = node['format'].value().width()
         new_height = node['format'].value().height()
 
+        # set frame range
         nuke.root()['first_frame'].setValue(first_frame)
         nuke.root()['last_frame'].setValue(last_frame)
         if nuke.frame() not in range(first_frame, last_frame+1):
             nuke.frame(first_frame)
 
+        # set fps
         try:
             nuke.root()['fps'].setValue(round(node.metadata()['input/frame_rate'], 3))
         except: 
@@ -18,7 +23,7 @@ def set_project():
                         '''\n\nreason:'''
                         '''\nThe frame rate data doesn't exist for this plate.'''
                         )
-
+        # set format
         exist = False
         for format in nuke.formats():
             if int(new_width) == int(format.width()) and int(new_height) == int(format.height()):
@@ -31,6 +36,7 @@ def set_project():
             nuke.addFormat(f"{new_width} {new_height} Plate")
             nuke.root()['format'].setValue('Plate')
 
+    # only one
     else:
         nuke.message('Please select a ReadNode node (only <span style="color:#FF453A">one</span>).')
 
