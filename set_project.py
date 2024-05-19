d7f6b36aad362485515a14b2ee4e7c0095a0e910
Lundy Hu <lundyhu@outlook.com>
2024-05-19 22:51:08 +0800

    Add files via upload

--- set_project.py
+++ set_project.py
@@ -1,3 +1,6 @@
+# URL: https://github.com/isLundy/Set-Project
+# version: 1.0
+
 def set_project():
 
     # check node numbers
@@ -6,40 +9,62 @@ def set_project():
         node = nuke.selectedNode()
         first_frame = 1001
         last_frame = int(node['origlast'].getValue() - node['origfirst'].getValue() + first_frame)
-        new_width = node['format'].value().width()
-        new_height = node['format'].value().height()
+        plate_width = int(node['format'].value().width())
+        plate_height = int(node['format'].value().height())
+        plate_pixelAspect = int(node['format'].value().pixelAspect())
 
         # set frame range
         nuke.root()['first_frame'].setValue(first_frame)
         nuke.root()['last_frame'].setValue(last_frame)
 
         # Move the current frame to the frame range
-        if nuke.frame() not in range(first_frame, last_frame+1):
+        if int(nuke.frame()) not in range(first_frame, last_frame+1):
             nuke.frame(first_frame)
+
+        # set format
+        existing_format = False
+        formatNames = [format.name() for format in nuke.formats()]
+
+        for format in nuke.formats():
+            if (plate_width == int(format.width()) and
+                plate_height == int(format.height()) and
+                plate_pixelAspect == int(format.pixelAspect()) and
+                format.name() and
+                formatNames.count(format.name()) == 1):
+
+                existing_format = True
+                nuke.root()['format'].setValue(format.name())
+                break
+
+        if existing_format == False:
+            if 'Plate' in formatNames:
+                n = 2
+                while f'Plate_{n}' in formatNames:
+                    n += 1
+                plate_name = f'Plate_{n}'
+
+            else:
+                plate_name = 'Plate'
+
+            nuke.addFormat(f"{plate_width} {plate_height} {plate_pixelAspect} {plate_name}")
+            nuke.root()['format'].setValue(plate_name)
 
         # set fps
         try:
             nuke.root()['fps'].setValue(round(node.metadata()['input/frame_rate'], 3))
-        except: 
-            nuke.message('''Please set <span style="color: #FF9F0A">fps</span> manually in the <span style="color: #FF9F0A">Project Settings</span>.'''
-                        '''\n\nreason:'''
-                        '''\nThe frame rate data doesn't exist for this plate.'''
-                        )
-        # set format
-        exist = False
-        for format in nuke.formats():
-            if int(new_width) == int(format.width()) and int(new_height) == int(format.height()):
-                if format.name():
-                    exist = True
-                    nuke.root()['format'].setValue(format.name())
-                    break
-
-        if exist == False:
-            nuke.addFormat(f"{new_width} {new_height} Plate")
-            nuke.root()['format'].setValue('Plate')
-
+        except:
+            metadata_fps = [value for key, value in node.metadata().items() if 'frame_rate' in key]
+            if metadata_fps:
+                nuke.root()['fps'].setValue(round(metadata_fps[0], 3))
+                print(metadata_fps)
+            else:
+                nuke.message('''Please set <span style="color: #FF9F0A">fps</span> manually in the <span style="color: rgb(255,159,10)">Project Settings</span>.'''
+                            '''\n\nreason:'''
+                            '''\nThe frame rate data doesn't exist.'''
+                            )
+                nuke.showSettings()
     # only one
     else:
-        nuke.message('Please select a ReadNode node (only <span style="color:#FF453A">one</span>).')
+        nuke.message('Please select a ReadNode node (only <span style="color: rgb(255,69,58)">one</span>).')
 
 set_project()
